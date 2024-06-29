2e9db85e2a224a3f893e2aa0a2c0bbea1e890f40
hannington owiti <hanningtonowiti@gmail.com>
2024-06-29 06:59:47 +0300

    functionsContinued

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate")
+{
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun("play soccer"));
+
+
+//LAB 2
+
+const mondayWork = function(activity = "go to the office")
+{
+    return `This Monday, I will ${activity}.`;
+};
+
+console.log(mondayWork());
+
+//LAB 3
+
+function wrapAdjective(flair = "someone")
+{
+    return function(adjective = "great") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
